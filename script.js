ad0e16b1d4dac4c321a17923fba5adbc56ff1d8f
Carina Meireles <74496779+cfcmeireles@users.noreply.github.com>
2023-02-05 13:00:06 +0000

    added event to remove active state on current target

--- script.js
+++ script.js
@@ -9,9 +9,15 @@
   let rating = 0;
 
   nums.forEach((num) => {
-    num.addEventListener("click", () => {
+    num.addEventListener("click", (event) => {
       rating = num.value;
-    });
+      nums.forEach(num => {
+        if (num !== event.currentTarget) {
+          num.classList.remove("active");
+        }
+      });
+      num.classList.add("active");
+      })
   });
 
   submit_btn.addEventListener("click", () => {
